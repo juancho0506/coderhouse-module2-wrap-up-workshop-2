ba014b4edf0eff8cff182ce7ce4ccc867c966925
Juan Torres <juane@backbase.com>
2023-04-14 17:45:33 -0500

    Added get current student endpoint

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -10,10 +10,22 @@ const router = Router();
 
 //Proteger estas vistas
 router.get('/', passportCall('jwt'), async(req,res)=>{
+    const student = req.user;
+    console.log("Estudiante logueado: ");
+    console.log(student);
     let students = await studentService.getAll();
     console.log(students);
     res.render('students',{students: students})
-})
+});
+
+router.get('/student', passportCall('jwt'), async(req,res)=>{
+    const student = req.user;
+    console.log("Estudiante logueado: ");
+    console.log(student);
+    let students = new Array();
+    students.push(student);
+    res.render('students',{students: students});
+});
 
 router.get('/courses', passportCall('jwt'), async(req,res)=>{
     let courses = await courseService.getAll();
